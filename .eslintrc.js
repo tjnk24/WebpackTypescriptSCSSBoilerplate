d3c0bf02e9ea770951cea5058af2566c9f1e8392
tjnk24 <tjnk24@gmail.com>
2020-08-19 08:39:08 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2020": true
+    },
+    "extends": [
+        "airbnb"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        'import/no-unresolved': 'off',
+        'import/extensions': 'off',
+    }
+};
