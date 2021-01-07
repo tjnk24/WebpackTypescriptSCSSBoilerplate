de365302f1b6cfdbd326cd8564a732fa1b6755b6
tjnk24 <tjnk24@gmail.com>
2021-01-07 16:18:17 +0300

    upgrading dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,23 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2020": true
-    },
-    "extends": [
-        "airbnb"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-        'import/no-unresolved': 'off',
-        'import/extensions': 'off',
-    }
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    indent: ['error', 2],
+    'no-unused-vars': 'warn',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+  },
 };
